544585adb4343899bdd1a2aee557ae46e43ba766
Helder Cambuta <heldercambuta44@gmail.com>
2024-09-02 00:50:57 +0100

    update the base app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoHeaderComponent } from './todo-header/todo-header.component';
+import { TodoItemComponent, type InputTask } from './todo-item/todo-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TodoHeaderComponent, TodoItemComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'udemy-course';
+  tasks: InputTask[] = [
+    {
+      id: 'sfdfdfdfd',
+      title: 'Estudar Angular 18',
+      status: 'pending',
+      created_at: new Date(),
+    },
+    {
+      id: 'sfdfdfdfd',
+      title: 'Estudar SSR no Angular',
+      status: 'pending',
+      created_at: new Date(),
+    },
+  ];
 }
